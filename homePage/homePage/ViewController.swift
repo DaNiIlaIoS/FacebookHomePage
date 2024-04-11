1b8e0c0aad5425f0505018cf89dc8ae5077d034f
Daniil <sivozelezovdaniil@gmail.com>
2024-04-11 13:40:02 +0500

    FacebookHomePage
    проект фейсбук с landscape версией и использованием Scroll View

---
+++ homePage/homePage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  homePage
+//
+//  Created by Даниил Сивожелезов on 01.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myAvatar: UIImageView!
+    @IBOutlet weak var friendOne: UIImageView!
+    @IBOutlet weak var friendTwo: UIImageView!
+    @IBOutlet weak var friendThree: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+    
+
+    
+}
+
